aad23bafe7cf5b7c658a02d868f1e095a027a483
Manu <manuelladcsilvacruz@gmail.com>
2022-02-11 23:22:50 -0300

    Pegando mensagens a cada 3 segundos com problema na rolagem.

--- src/script.js
+++ src/script.js
@@ -1,3 +1,85 @@
+let messages = [];
+let filteredMsgs = [];
+
+let msg = {
+    from: "Manu",
+    to: "Todos",
+    text: "Boa noite!",
+    type: "message",
+    time: "01:10:44"
+};
+
+let usuario = {
+    from: "teste",
+    to: "Todos",
+    text: "Boa noite!",
+    type: "message",
+    time: "01:10:44"
+};
+
+getMessages();
+setInterval(getMessages, 3000);
+
+function getMessages(){
+    const promise = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
+    promise.then(getData);
+    console.log("Voltei do then!");
+    const ultimaMensagem = document.querySelector("main").lastElementChild;
+    ultimaMensagem.scrollIntoView();
+}
+  
+function getData(answerServidor) {
+    console.log("Entrei em getData()");
+    for (let i=0; i<answerServidor.data.length; i++){
+        messages.push(answerServidor.data[i]);
+    }
+    // showAllMessages(messages);
+    filterUserMsgs();
+}
+
+function showAllMessages(msgList) {
+    for(let i=0; i<msgList.length; i++){
+        screenRenderMessage(msgList[i]);
+    }
+}
+
+function filterUserMsgs(){
+    const userMsgs = messages.filter(filterMsg);
+    userMsgs.forEach(screenRenderMessage);
+}
+
+function filterMsg(msg){
+    if (msg.from === usuario.from || msg.to === usuario.from){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function screenRenderMessage(msg) {
+    const chat = document.querySelector("main");
+    if (msg.type === "message"){
+        chat.innerHTML += `
+        <div data-identifier="message" class="message normal">
+            <p class="time">${msg.time}</p>
+            <p> <span class="nome-usuario">${msg.from} </span> para <span class="nome-usuario">${msg.to}</span>: ${msg.text}</p>
+        </div>`;
+    }else if (msg.type == "status"){
+        chat.innerHTML += `
+        <div data-identifier="message" class="message status">
+            <p class="time">${msg.time}</p>
+            <p> <span class="nome-usuario">${msg.from} </span> ${msg.text}</p>
+        </div>`;
+    }else if (msg.type == "private_message"){
+        chat.innerHTML += `
+        <div class="message reserved">
+            <p class="time">${msg.time}</p>
+            <p> <span class="nome-usuario">${msg.from} </span> reservadamente para <span class="nome-usuario">${msg.to}</span>: ${msg.text}</p>
+        </div>`;
+    }
+    chat.scrollIntoView();
+}
+
 function login(nome){
     const usuario = {
         usuario: nome
@@ -6,22 +88,4 @@ function login(nome){
     promise.then(imprimirMsg);   
 }
 
-buscarMensagens();
-
-function iniciarChat(){
-
-}
-
-function buscarMensagens(){
-    const promise = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
-    promise.then(imprimirMsg);
-}
-
-function imprimirMsg(msg){
-    console.log(msg.data);
-
-}
-
 function enviarMsg(){}
-
-function manterConcexao(){}
