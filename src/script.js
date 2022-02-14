9e7b2b08ac3422ee1478b612786ae3ce797c4f5c
Manu <manuelladcsilvacruz@gmail.com>
2022-02-14 14:36:18 -0300

    Funcao de envio de msg, atualizacao de status e lista de msgs incluidas

--- src/script.js
+++ src/script.js
@@ -2,9 +2,12 @@
 let messages = [];
 let filteredMsgs = [];
 
+let messageType = "";
+
 
 let today = new Date();
 let currentTime = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+let lastTimeMsg = "";
 
 let msg = {
     from: "",
@@ -18,34 +21,29 @@ let usuario = {
     name: ""
 };
 
- 
+// Function area
 function getData(answerServidor) {
+    messages = [];
     console.log("Entrei em getData()");
     for (let i=0; i<answerServidor.data.length; i++){
         messages.push(answerServidor.data[i]);
     }
-    showAllMessages(messages);
-    // filterUserMsgs();
+    filterUserMsgs();
 
     // Makes the last message scroll up
     const lastMsg = document.querySelector("main").lastElementChild;
     lastMsg.scrollIntoView();
-    let lastTimeMsg = lastMsg.querySelector(".time").innerHTML;
-}
-
-function showAllMessages(msgList) {
-    for(let i=0; i<msgList.length; i++){
-        screenRenderMessage(msgList[i]);
-    }
+    lastTimeMsg = lastMsg.querySelector(".time").innerHTML;
 }
 
 function filterUserMsgs(){
     const userMsgs = messages.filter(filterMsg);
-    userMsgs.forEach(screenRenderMessage);
+    setTimeout(userMsgs.forEach(screenRenderMessage), 50);
 }
 
+// Falta verificar se as msgs são de depois que o usuário entrou
 function filterMsg(msg){
-    if (msg.from === usuario.nome || msg.to === usuario.nome || msg.to === "Todos"){
+    if (msg.from === usuario.name || msg.to === usuario.name || msg.to === "Todos"){
         return true;
     }else{
         return false;
@@ -54,26 +52,74 @@ function filterMsg(msg){
 
 function screenRenderMessage(msg) {
     const chat = document.querySelector("main");
-    if (msg.type === "message"){
-        chat.innerHTML += `
-        <div data-identifier="message" class="message normal">
-            <p class="time">${msg.time}</p>
-            <p> <span class="nome-usuario">${msg.from} </span> para <span class="nome-usuario">${msg.to}</span>: ${msg.text}</p>
-        </div>`;
-    }else if (msg.type == "status"){
-        chat.innerHTML += `
-        <div data-identifier="message" class="message status">
-            <p class="time">${msg.time}</p>
-            <p> <span class="nome-usuario">${msg.from} </span> ${msg.text}</p>
-        </div>`;
-    }else if (msg.type == "private_message"){
-        chat.innerHTML += `
-        <div class="message reserved">
-            <p class="time">${msg.time}</p>
-            <p> <span class="nome-usuario">${msg.from} </span> reservadamente para <span class="nome-usuario">${msg.to}</span>: ${msg.text}</p>
-        </div>`;
+    if (usuario.name == msg.from){
+        if (msg.type === "message"){
+            chat.innerHTML += `
+            <div data-identifier="message" class="message normal">
+                <p class="time">${msg.time}</p>
+                <p> <span class="nome-usuario">Você </span> para <span class="nome-usuario">${msg.to}</span>: ${msg.text}</p>
+            </div>`;
+        }else if (msg.type == "status"){
+            chat.innerHTML += `
+            <div data-identifier="message" class="message status">
+                <p class="time">${msg.time}</p>
+                <p> <span class="nome-usuario">Você </span> ${msg.text}</p>
+            </div>`;
+        }else if (msg.type == "private_message"){
+            chat.innerHTML += `
+            <div class="message reserved">
+                <p class="time">${msg.time}</p>
+                <p> <span class="nome-usuario">Você </span> reservadamente para <span class="nome-usuario">${msg.to}</span>: ${msg.text}</p>
+            </div>`;
+        }
+    }else if (usuario.name == msg.to){
+        if (msg.type === "message"){
+            chat.innerHTML += `
+            <div data-identifier="message" class="message normal">
+                <p class="time">${msg.time}</p>
+                <p> <span class="nome-usuario">${msg.from} </span> para <span class="nome-usuario">Você</span>: ${msg.text}</p>
+            </div>`;
+        }else if (msg.type == "status"){
+            chat.innerHTML += `
+            <div data-identifier="message" class="message status">
+                <p class="time">${msg.time}</p>
+                <p> <span class="nome-usuario">${msg.from} </span> ${msg.text}</p>
+            </div>`;
+        }else if (msg.type == "private_message"){
+            chat.innerHTML += `
+            <div class="message reserved">
+                <p class="time">${msg.time}</p>
+                <p> <span class="nome-usuario">${msg.from} </span> reservadamente para <span class="nome-usuario">Você</span>: ${msg.text}</p>
+            </div>`;
+        }
+    }else{
+        if (msg.type === "message"){
+            chat.innerHTML += `
+            <div data-identifier="message" class="message normal">
+                <p class="time">${msg.time}</p>
+                <p> <span class="nome-usuario">${msg.from} </span> para <span class="nome-usuario">${msg.to}</span>: ${msg.text}</p>
+            </div>`;
+        }else if (msg.type == "status"){
+            chat.innerHTML += `
+            <div data-identifier="message" class="message status">
+                <p class="time">${msg.time}</p>
+                <p> <span class="nome-usuario">${msg.from} </span> ${msg.text}</p>
+            </div>`;
+        }else if (msg.type == "private_message"){
+            chat.innerHTML += `
+            <div class="message reserved">
+                <p class="time">${msg.time}</p>
+                <p> <span class="nome-usuario">${msg.from} </span> reservadamente para <span class="nome-usuario">${msg.to}</span>: ${msg.text}</p>
+            </div>`;
+        }
     }
     chat.scrollIntoView();
+}
+
+function showAllMessages(msgList) {
+    for(let i=0; i<msgList.length; i++){
+        screenRenderMessage(msgList[i]);
+    }
 }
 
 function loadingLogin(){
@@ -89,8 +135,8 @@ function login(){
     promise.then(loginOk);
     promise.catch(loginError);
 
-    getMessages();
-    setInterval(pingMsg, 5000);
+    let pMsg = setInterval(getMessages, 3000);
+    let pUser = setInterval(pingUser, 5000);
 }
 
 function loginOk(){
@@ -99,7 +145,7 @@ function loginOk(){
 }
 
 function loginError(error){
-    alert("ERROR:" + error);
+    alert(error);
     usuario.nome = null;
     document.querySelector(".carregando").style.display = "none";
     document.querySelector(".login").querySelector("input").value = "Digite seu nome";
@@ -107,9 +153,9 @@ function loginError(error){
 }
 
 function getMessages(){
+    console.log("Ping messages");
     const promise = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
     promise.then(getData);
-    console.log("Voltei do then!");
 }
 
 function openSidebar(){
@@ -122,7 +168,42 @@ function closeSidebar(){
     document.querySelector("aside").style.display = "none";
 }
 
-function pingMsg(){
+function pingUser(){
     const promise = axios.post("https://mock-api.driven.com.br/api/v4/uol/status", usuario);
     console.log("Pin servidor");
+    usuario.name = "";
+    promise.catch(messageError);
+}
+
+document.addEventListener("keypress", function(element) {
+    if(element.key === 'Enter') {
+        var button = document.querySelector("#submit");
+        button.sendMessage();
+    }
+});
+
+function sendMessage(){
+    console.log("Cliquei para enviar msg!");
+    msg = createMessage();
+    console.log(msg);
+    const promise = axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", msg);
+    promise.catch(messageError);
+}
+
+function messageError(){
+    alert("Seu usuário foi desconectado!");
+    document.location.reload(true);
+}
+
+function createMessage(){
+    let texto = document.querySelector("input").value;
+    console.log(texto + lastTimeMsg);
+    msg = {
+        from: usuario.name,
+        to: "Todos",
+        text: texto,
+        type: "message",
+        time: lastTimeMsg
+    };
+    return msg;
 }
